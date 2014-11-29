37013e1f3daf27f22ddf4705983a7e9450928984
parkhomenko <dmitriy.parkhomenko@icloud.com>
2014-11-29 13:27:35 +0200

    price buttons

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -15,9 +15,9 @@ $(document).ready(function(){
      $('.owl-controls .clickable .owl-page span').remove(); 
      $('.owl-page').addClass('col-md-4'); 
     
-     $('.owl-controls .owl-page:first').append('<div class=""><button class="btn btn-lg btn-warning">Стандарт</button></div>');
-     $('.owl-controls .owl-page:nth-child(2)').append('<div class=""><button class="btn btn-lg btn-warning">Бизнес</button></div>');
-     $('.owl-controls .owl-page:last').append('<div class=""><button class="btn btn-lg btn-warning">Универсал</button></div>');
+     $('.owl-controls .owl-page:first').append('<div class=""><button class="btn btn-lg price_btn">Стандарт</button></div>');
+     $('.owl-controls .owl-page:nth-child(2)').append('<div class=""><button class="btn btn-lg price_btn">Бизнес</button></div>');
+     $('.owl-controls .owl-page:last').append('<div class=""><button class="price_btn">Универсал</button></div>');
     
 //     $('.owl-controls .owl-page').append('<div class=""><button class="btn btn-lg btn-warning">Warning</button></div>');       
 //     $('.owl-controls .clickable .owl-page').append('<div class=""><button class="btn btn-lg btn-warning">Warning</button></div>');       
