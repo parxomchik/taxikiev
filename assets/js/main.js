9153c027020d4e962e462b362e18d02d305dfbdc
parkhomenko <dmitriy.parkhomenko@icloud.com>
2015-01-07 19:01:05 +0200

    добавил дополнительные услуги и активную кнопку

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-    $(document).ready(function(){
-        $(".price_btn").click(function() {
-            $(".price_btn").removeClass("price_active");
-            $(this).addClass("price_active");
-        });
-    });
--- assets/js/slider.js
+++ assets/js/slider.js
@@ -7,7 +7,21 @@ $(document).ready(function() {
       pagination:  false,
       singleItem:true,
       slideSpeed : 300,
-      paginationSpeed : 400
+      paginationSpeed : 400,
+      afterMove : function (elem) {
+           if(this.currentItem === this.maximumItem){
+           $(".price_btn").removeClass("price_active");
+           $(".price_btn").last().addClass("price_active");
+        }
+           if(this.currentItem === 0){
+           $(".price_btn").removeClass("price_active");
+           $(".price_btn").first().addClass("price_active");   
+           }
+           if(this.currentItem === 1){
+           $(".price_btn").removeClass("price_active");
+           $(".customNavigation div:nth-child(2) a").addClass("price_active");   
+           }
+      }
 //      itemsDesktop : [1000,5], //5 items between 1000px and 901px
 //      itemsDesktopSmall : [900,3], // betweem 900px and 601px
 //      itemsTablet: [600,2], //2 items between 600 and 0
@@ -24,5 +38,8 @@ $(document).ready(function() {
     $("#universal").click(function(){
     owl.trigger('owl.goTo',2);
   })
-  
+  $(".price_btn").click(function() {
+    $(".price_btn").removeClass("price_active");
+    $(this).addClass("price_active");
+  });
 });
